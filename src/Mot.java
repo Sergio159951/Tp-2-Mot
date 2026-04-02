eb2c299de5b932c021d0053e3a0242d87a2cffe8
Sergio Serrano <sergiocastellar16@gmail.com>
2026-04-01 21:38:13 -0400

    Mot fini, non verifie

--- src/Mot.java
+++ src/Mot.java
@@ -1,5 +1,7 @@
 // VOUS POUVEZ MODIFIER CE FICHIER
 
+import java.util.Arrays;
+
 public class Mot {
     private char[] lettres;
     private int nbLettres;
@@ -19,6 +21,59 @@ public class Mot {
 
     // AJOUTEZ VOTRE CODE CI-DESSOUS
 
+    public int getLongueur() {
+        return this.nbLettres;
+    }
+
+    public char getLettre(int index) {
+        if (index < 0 || index >= this.nbLettres) {
+            return '\0';
+        }
+        return this.lettres[index];
+    }
+
+    public void ajouter(char c) {
+        char[] newLettres = new char[this.nbLettres + 1];
+        for (int i = 0; i < this.nbLettres; i++) {
+            newLettres[i] = this.lettres[i];
+        }
+        newLettres[this.nbLettres] = c;
+        this.lettres = newLettres;
+        this.nbLettres++;
+    }
 
 
+    public boolean inserer(char c, int index) {
+        if (index < 0 || index > this.nbLettres) {
+            return false;
+        }
+
+        char[] newLettres = new char[this.nbLettres + 1];
+
+        for (int i = 0; i < index; i++) {
+            newLettres[i] = this.lettres[i];
+        }
+
+        newLettres[index] = c;
+
+        for (int i = index; i < this.nbLettres; i++) {
+            newLettres[i + 1] = this.lettres[i];
+        }
+
+        this.lettres = newLettres;
+        this.nbLettres++;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String resultat = "";
+
+        for (int i = 0; i < lettres.length; i++) {
+            resultat += lettres[i];
+        }
+
+        return resultat;
+    }
 }
